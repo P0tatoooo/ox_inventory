fbf40a433419f2c3a73344bbb09a8da8069274ba
Potato <raphael.mittler@gmail.com>
2024-08-15 16:21:34 +0200

    Add bulletproof

--- data/shops.lua
+++ data/shops.lua
@@ -229,6 +229,7 @@ return {
             {name = 'bulletproof', grade = 1, metadata = {bulletproofvesttype = 2, label = "Gilet Cadet", image = "bulletproof_rookie" }, price = 0},
             {name = 'bulletproof', grade = 1, metadata = {bulletproofvesttype = 3, label = "Gilet Patrouille", image = "bulletproof_lspd" }, price = 0},
             {name = 'bulletproof', grade = 1, metadata = {bulletproofvesttype = 4, label = "Gilet Tactique", image = "bulletproof_tactical" }, price = 0},
+            {name = 'bulletproof', grade = 1, metadata = {bulletproofvesttype = 8, label = "Gilet Tactique Lourd", image = "bulletproof_tactical_heavy" }, price = 0},
             {name = 'bulletproof', grade = 1, metadata = {bulletproofvesttype = 5, label = "Gilet Media", image = "bulletproof_media" }, price = 0},
             {name = 'bulletproof', grade = 1, metadata = {bulletproofvesttype = 6, label = "Gilet Medic", image = "bulletproof_medic" }, price = 0},
 
