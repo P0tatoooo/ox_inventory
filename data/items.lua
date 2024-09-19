516eed5ae0ff2f3e20c7c670ea9219f7f09602eb
Raphael Mittler <raphael.mittler@gmail.com>
2024-09-19 15:00:24 +0200

    Update items

--- data/items.lua
+++ data/items.lua
@@ -54,20 +54,20 @@ return {
 
 	['bandage'] = {
 		label = 'Bandage',
-		weight = 115,
+		weight = 100,
 		stack = true,
 	},
 
 	['pharmacybandage'] = {
-		label = 'Bandage',
-		weight = 115,
+		label = 'Pansement',
+		weight = 100,
 		stack = true,
-		client = {
+		--[[ client = {
 			anim = { dict = 'missheistdockssetup1clipboard@idle_a', clip = 'idle_a', flag = 49 },
 			prop = { model = 'prop_rolled_sock_02', pos = vec3(-0.14, -0.14, -0.08), rot = vec3(-50.0, -50.0, 0.0) },
 			disable = { move = true, car = true, combat = true },
 			usetime = 2500,
-		}
+		} ]]
 	},
 
 	['parachute'] = {
