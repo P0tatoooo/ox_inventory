33bc2b4525eb91d4ee6407e20d21292101605fa1
Potato <44370952+P0tatoooo@users.noreply.github.com>
2024-11-02 15:59:24 +0100

    Add items

--- data/items.lua
+++ data/items.lua
@@ -822,6 +822,44 @@ return {
             }
         },
 
+		['ribs'] = {
+            label = 'Ribs Caramélisés',
+            weight = 200,
+            stack = true,
+            close = true,
+            degrade = 4320,
+            client = {
+                status = { hunger = 25},
+                anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger_fp' },
+                prop = {
+                    model = 'prop_cs_burger_01',
+                    pos = { x = 0.02, y = 0.02, z = -0.02},
+                    rot = { x = 0.0, y = 0.0, z = 0.0}
+                },
+                usetime = 2500,
+				export = 'MyCity_CoreV2.eatFood'
+            }
+        },
+
+		['tiramisu'] = {
+            label = 'Tiramisu',
+            weight = 200,
+            stack = true,
+            close = true,
+            degrade = 4320,
+            client = {
+                status = { hunger = 25},
+                anim = { dict = 'mp_player_inteat@burger', clip = 'mp_player_int_eat_burger_fp' },
+                prop = {
+                    model = 'prop_cs_burger_01',
+                    pos = { x = 0.02, y = 0.02, z = -0.02},
+                    rot = { x = 0.0, y = 0.0, z = 0.0}
+                },
+                usetime = 2500,
+				export = 'MyCity_CoreV2.eatFood'
+            }
+        },
+
 		['applepie'] = {
             label = 'Tarte aux Pommes',
             weight = 200,
